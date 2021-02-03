43dc1b40f622d740b56b4beed3ff8cd3b9a78386
pandabearcoding <madelinerose16@outlook.com>
2021-02-03 12:03:57 -0500

    week 4, commit 4: Built Actions for User and Favorite Controllers

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,2 +1,32 @@
 class FavoritesController < ApplicationController
+    def index
+        favorites = Favorite.all
+        render json: favorites   
+    end
+
+    # do we need a show method if we could just link to the product's show page?
+
+    def update
+        favorite = Favorite.find(params[:id])
+        favorite.update!(favorite_params)
+        render json: {}
+    end 
+
+    def create
+        favorite = Favorite.create!(favorite_params)
+        render json: favorite
+    end
+
+    def destroy
+        favorite = Favorite.find(params[:id])
+        favorite.destroy
+        render json: {}
+    end 
+
+    private
+    def favorite_params
+        params.require(:favorite).permit(:user_id, :product_id)
+    end
 end
+
+  
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,50 @@
 class UsersController < ApplicationController
+    # to be able to view and interact with other members?
+    def index
+        users = User.all
+        render json: users
+    end 
+
+    def show
+        user = User.find(params[:id])
+        render json: user, except: [:created_at, :updated_at]
+    end
+
+    def create
+        user = User.create!(user_params)
+        render json: user
+    end 
+
+    def update
+        user = User.find(params[:id])
+        user.update(user_params)
+        render json: user
+    end 
+
+    def destroy
+        user = User.find(params[:id])
+        user.destroy
+        render json: {}
+    end 
+
+    # list of products the user favorited 
+    # def favorite_products
+    #     user = User.find(params[:id])
+    #     favorites = user.favorites
+    #     render json: favorites
+    # end
+
+    # list of products the user put in their cart
+    # def cart_products
+    #     user = User.find(params[:id])
+    #     carts = user.carts
+    #     render json: carts
+    # end 
+
+    # will need login/logout actions for Auth
+
+    private 
+    def user_params
+        params.require(:user).permit(:username, :password, :name, :profile_picture, :phone_number, :email, :address, :cc_number, :cc_date, :cc_cvv)
+    end
 end
