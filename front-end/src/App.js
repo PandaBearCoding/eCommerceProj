6f424ab36f372bc2f1988f6eae982bac48babacf
pandabearcoding <madelinerose16@outlook.com>
2021-02-17 15:55:47 -0500

    week 6, commit 10: Added dummy seeds for User, Product, Cart, and Favorite

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -6,13 +6,30 @@ import FavoriteContainer from './Containers/FavoriteContainer.js';
 import ProductContainer from './Containers/ProductContainer.js';
 
 class App extends React.Component {
+  
+  // current user will need to be held in state in order to access their favorites/cart
+  state = {
+    user: {}
+  }
+
+  // will need to fetch current user to access their favorites/cart
+  // componentDidMount(){
+  //   fetch(`http://localhost:3001/users/${user.id}`)
+  //   .then(resp => resp.json())
+  //   .then(user => (this.setState({user: user})))
+  //   .catch(console.log)
+  // }
+
+  matchDeleteHandler = (cartObj) => {
+    console.log("Cart Item To Delete", cartObj.id)
+  }
 
   render(){
     return(
       <div className="App">
         {/* <TestCard /> */}
         <UserContainer />
-        <CartContainer />
+        <CartContainer cartDeleteHandler={this.cartDeleteHandler} />
         <FavoriteContainer />
         <ProductContainer />
       </div>
